ca81afda4ffa80a4ff120f1b6636f3bc966a05f0
thatLeaflet <80165419+thatLeaflet@users.noreply.github.com>
2022-05-23 19:32:12 -0400

    also change GTK3 theme

--- change-theme.sh
+++ change-theme.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# put then names of your GTK3 themes here
+# for example, here's mine
+LIGHT_GTK3_THEME='Yaru-purple'
+DARK_GTK3_THEME='Yaru-purple-dark'
+
+
 CURRENT_THEME=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 if [[ "$CURRENT_THEME" == "'prefer-light'" ]]; then
 	gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+	gsettings set org.gnome.desktop.interface gtk-theme "$DARK_GTK3_THEME"
 else
 	gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
+	gsettings set org.gnome.desktop.interface gtk-theme "$LIGHT_GTK3_THEME"
 fi
